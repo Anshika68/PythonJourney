7e99e672918907ebd089eb408c870083c3f1210c
Anshika <86038330+Anshika68@users.noreply.github.com>
2025-05-23 13:19:52 +0530

    Update Day1_Band_NameGenerator.py

--- Day1_Band_NameGenerator.py
+++ Day1_Band_NameGenerator.py
@@ -1,1 +1,10 @@
-
+# Day 1
+# Band Name Generator
+print("Welcome to the Band Name Generator.")
+# Taking city name as input
+city = input("What's the name of the city you grew up in?\n")
+# Taking pet name as input
+pet_name = input("What's your pet's name?\n")
+# Craeting the band name
+band_name = city + " " + pet_name
+print(f"Your band name could be {band_name}")
